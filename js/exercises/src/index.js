cda1d5e310ce300f9c9d3c8b526750963f24ae0f
Ana Maria Sosa <anamariasosam@gmail.com>
2017-02-07 11:14:11 -0500

    Add react router

--- js/exercises/src/index.js
+++ js/exercises/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, Link } from 'react-router'
 import App from './App';
+import convert from './convertorIEEE';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const NoMatch = () => (
+  <p>No Match <Link to='/'>go back</Link></p>
+)
+
+ReactDOM.render((
+  <Router history={browserHistory} >
+    <Route path="/" component={App} />
+    <Route path="/ieee" component={convert}/>
+    <Route path="*" component={NoMatch}/>
+  </Router>
+), document.getElementById('root'));
