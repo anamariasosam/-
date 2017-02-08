45df847e8857caae7eec7c111ef4055ecd2e9326
Ana Maria Sosa <anamariasosam@gmail.com>
2017-02-08 13:46:52 -0500

    add mantiza

--- js/exercises/src/Convertor.js
+++ js/exercises/src/Convertor.js
@@ -7,7 +7,7 @@ export default class Convertor {
 	toBin() {
     let n = Number(this.number).toString(2);
     console.log('Binario:', n);
-		return n;
+		return Math.abs(n).toString();
 	};
 
   getIntegerPart() {
@@ -37,50 +37,61 @@ export default class Convertor {
 
   setEightBits() {
     if (this.getIntegerPart() > 0) {
-      let array = this.getIntegerPart().split("").map(Number);
-      let index = 0;
-
-      for (let i = 0; i < array.length; i++) {
-        if (array[i] === 1) {
-          index = i + 1;
-          break;
-        }
-      }
-
-      let exponent = (array.length - index) + 127;
-      array.splice(index - 1, 1);
-      console.log('array', array);
-      this.binary32Bits[1] = Number(exponent).toString(2);
+      this.setIntegerPartEightBits();
     } else {
-      let array = this.getFloatPart().split("").map(Number);
-      let index = 0;
-
-      for (let i = 0; i < array.length; i++) {
-        if (array[i] === 1) {
-          index = i + 1;
-          break;
-        }
-      }
-      let exponent = 127 - index;
-      this.binary32Bits[1] = Number(exponent).toString(2);
+      this.setFloatPartEightBits();
     }
-
   }
 
-  fillMantizaWithZeros() {
-    let zeros = []
-    for (var i = 0; i < 22; i++) {
-      zeros[i] = 0
-    }
-    // aqui es lo que sige usando lo de arriba
-    this.binary32Bits[3] = zeros.join("").toString();
-  }
+	setIntegerPartEightBits() {
+		let array = this.getIntegerPart().split("").map(Number);
+		const value = this.getIndex(array);
+		const exponent = 127 + (array.length - value);
+		this.binary32Bits[1] = Number(exponent).toString(2);
+
+		this.binary32Bits[2] = this.setTwentyThreeBits(array, value);
+
+	}
+
+	setTwentyThreeBits(array, value) {
+		array.splice(value - 1, 1);
+		let mantiza = array.join("");
+		let mantiza23bits = new Array(23).fill(0);
+
+		if (!(this.getFloatPart() === undefined)) {
+			 mantiza += this.getFloatPart();
+		}
+		
+		mantiza.split("")
+		for (let i = 0; i < mantiza.length; i++) {
+			mantiza23bits[i] = mantiza[i];
+		}
+
+		return mantiza23bits.join("")
+	}
+
+	setFloatPartEightBits() {
+		let array = this.getFloatPart().split("").map(Number);
+		const value = this.getIndex(array);
+		const exponent = 127 - value;
+		this.binary32Bits[1] = Number(exponent).toString(2);
+
+		let mantiza = new Array(23).fill(0);
+		this.binary32Bits[2] = mantiza.join("")
+	}
+
+	getIndex(array) {
+		for (let i = 0; i < array.length; i++) {
+			if (array[i] === 1) {
+				return i + 1;
+			}
+		}
+	}
 
   getBinaryNumber() {
     this.setFirstBit();
     this.setEightBits();
-    this.fillMantizaWithZeros();
-    console.log(this.binary32Bits);
+    console.log('resultado final',this.binary32Bits);
     return this.binary32Bits.join(" ");
   }
 };
