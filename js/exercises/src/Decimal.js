8a9df0ab72902dd6532112fe07777e3707c1a428
Ana Maria Sosa <anamariasosam@gmail.com>
2017-02-10 13:01:57 -0500

    add binary to decimal

--- js/exercises/src/Decimal.js
+++ js/exercises/src/Decimal.js
@@ -9,26 +9,34 @@ export default class Decimal {
     console.log('Signo', this.decimal[0]);
   }
 
-  getBinary() {
+  getDecimal() {
     let a = this.binary.split("")
     let b = a.splice(1, 8).join("")
-    console.log('ocho biys', b);
+    console.log('ocho bits', b);
+    console.log('decimal', parseInt(b, 2));
     return parseInt(b, 2);
   }
 
   getExponent() {
-    console.log('Exponente', this.getBinary() - 127 );
-    return this.getBinary() - 127
+    console.log('Exponente', this.getDecimal() - 127 );
+    return this.getDecimal() - 127
   }
 
   getMantiza() {
     let a = this.binary.split("")
-    let b = a.splice(9, this.getExponent())
+    let b = a.splice(9, Math.abs(this.getExponent()))
+    console.log('mantiza', b.join(''));
     return b.join('');
   }
 
   setFirstPart() {
-    this.decimal[1] = parseInt('1' + this.getMantiza(), 2);
+    if (this.getExponent > 0) {
+      this.decimal[1] = parseInt('1' + this.getMantiza(), 2);
+    }
+    else {
+      let a  = this.getMantiza() + '.1'
+      // solo falta cuando es con coma saber como coger la mantiza
+    }
 
   }
 
